1d07d84cd6dfe326b42a1d136fa8916908bda87d
Amalendu B Hareesh <amalenduhareesh3@gmail.com>
2026-05-08 16:37:18 +0530

    Host Allowing

--- backend/settings.py
+++ backend/settings.py
@@ -4,7 +4,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'web-production-d1e3c.up.railway.app',
+    '127.0.0.1',
+    'localhost'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
